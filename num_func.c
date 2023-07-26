afd2e1c294ca3b30391b9fb00d432129a52aeb1e
exornamb <exornamb@gmail.com>
2023-07-26 00:21:23 -0700

    k

--- num_func.c
+++ num_func.c
@@ -44,30 +44,29 @@ int print_unsigned(va_list list, char ingre[],
  * @size: Size specifier
  * Return: Number of chars printed
 */
-int print_octal(va_list list, char ingre[],
-        int flags, int width, int precision, int size)
+int print_octal(va_list list, char ingre[],int flags, int width, int precision, int size)
 {
 
         int i = BUFF_SIZE - 2;
-        unsigned long int num = va_arg(list, unsigned long int);
-        unsigned long int init_num = num;
-        UNUSED(width);
+unsigned long int num = va_arg(list, unsigned long int);
+unsigned long int init_num = num;
+UNUSED(width);
 
         num = unsigned_size(num, size);
 
         if (num == 0)
-                ingre[i--] = '0';
+ingre[i--] = '0';
 
         ingre[BUFF_SIZE - 1] = '\0';
 
         while (num > 0)
-        {
-                ingre[i--] = (num % 8) + '0';
-                num /= 8;
-        }
+{
+ingre[i--] = (num % 8) + '0';
+num /= 8;
+}
 
         if (flags & F_HASH && init_num != 0)
-                ingre[i--] = '0';
+ingre[i--] = '0';
 
         i++;
 
